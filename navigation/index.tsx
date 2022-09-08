8fd3289faa476b78f8e932a757032fefc02136ec
xongjaemin <woals99@gmail.com>
2022-09-08 19:45:39 +0900

    style: 네브바, 탭바 레이아웃 개발 진행중

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -8,22 +8,27 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { ColorSchemeName, Pressable } from 'react-native';
+import { ColorSchemeName, Pressable, Image, StyleSheet, Text } from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
-import TabOneScreen from '../screens/TabOneScreen';
+import HomeTab from '../screens/HomeTab';
 import TabTwoScreen from '../screens/TabTwoScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
+
+import HomeIcon from '../assets/images/common/ic_home.png';
+import SearchIcon from '../assets/images/common/ic_search.png';
+import FoucesdIcon from '../assets/images/common/ic_focused.png';
+import PlusIcon from '../assets/images/common/ic_plus.png';
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
     <NavigationContainer
       linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      theme = {DarkTheme}>
       <RootNavigator />
     </NavigationContainer>
   );
@@ -60,26 +65,41 @@ function BottomTabNavigator() {
     <BottomTab.Navigator
       initialRouteName="TabOne"
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme].tint,
+        tabBarActiveBackgroundColor: 'black',
+        tabBarInactiveBackgroundColor: 'black',
+        tabBarLabel: ''
       }}>
       <BottomTab.Screen
         name="TabOne"
-        component={TabOneScreen}
+        component={HomeTab}
         options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'MOMENTOON',
+          headerStyle: {
+            backgroundColor: 'black',
+          },
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 28,
+          },
+          headerTitleAlign: 'left',
+          tabBarIcon: ({focused}) => { 
+            return (
+              <>
+              <Image source={HomeIcon} style={styles.tabIcon}></Image>
+              <Image source={FoucesdIcon} style={focused? styles.indicator : styles.hideIndicator}></Image>
+              </>
+              )},
           headerRight: () => (
             <Pressable
               onPress={() => navigation.navigate('Modal')}
               style={({ pressed }) => ({
                 opacity: pressed ? 0.5 : 1,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center'
               })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
+              <Image source={PlusIcon} style={{width: 18, height: 18, marginRight: 3}}/>
+              <Text style={styles.createBtn}>create</Text>
             </Pressable>
           ),
         })}
@@ -89,19 +109,38 @@ function BottomTabNavigator() {
         component={TabTwoScreen}
         options={{
           title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({focused}) => { 
+            return (
+              <>
+              <Image source={SearchIcon} style={styles.tabIcon}></Image>
+              <Image source={FoucesdIcon} style={focused? styles.indicator : styles.hideIndicator}></Image>
+              </>  
+            )},
         }}
       />
     </BottomTab.Navigator>
   );
 }
 
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
-}
+
+const styles = StyleSheet.create({
+  tabIcon: {
+    width: 32,
+    height: 32,
+    marginTop: 10
+  },
+  indicator: {
+    marginTop: 3,
+    width: 6,
+    height: 6
+  },
+  hideIndicator: {
+    display: 'none'
+  },
+  createBtn: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginRight: 15
+  }
+});
--- screens/HomeTab.tsx
+++ screens/HomeTab.tsx
@@ -1,15 +1,18 @@
 import { StyleSheet } from 'react-native';
+import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function HomeTab({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>홈화면</Text>
+      {/*
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+  */}
     </View>
   );
 }
@@ -19,8 +22,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'black'
   },
   title: {
+    color: 'white',
     fontSize: 20,
     fontWeight: 'bold',
   },
