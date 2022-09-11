411da592dee3e97574eb06d8f5a43de781d10d6e
xongjaemin <woals99@gmail.com>
2022-09-11 15:06:02 +0900

    feat: 파베 연동

---
+++ firebaseConfig.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+
+import "firebase/compat/database";
+import "firebase/compat/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBA8zu4FFQy8I75P2gAC4oil2wNwk70fpM",
+  authDomain: "momentoon-cd88f.firebaseapp.com",
+  databaseURL: "https://momentoon-cd88f-default-rtdb.firebaseio.com",
+  projectId: "momentoon-cd88f",
+  storageBucket: "momentoon-cd88f.appspot.com",
+  messagingSenderId: "870406035767",
+  appId: "1:870406035767:web:c13f7376b020aa4366e2b2",
+};
+
+// Initialize Firebase
+/*
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}*/
+firebase.initializeApp(firebaseConfig);
+
+export const firebase_db = firebase.database();
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,32 +3,50 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { FontAwesome } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as React from 'react';
-import { ColorSchemeName, Pressable, Image, StyleSheet, Text } from 'react-native';
+import { FontAwesome } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import {
+  ColorSchemeName,
+  Pressable,
+  Image,
+  StyleSheet,
+  Text,
+} from "react-native";
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import ModalScreen from '../screens/ModalScreen';
-import NotFoundScreen from '../screens/NotFoundScreen';
-import HomeTab from '../screens/HomeTab';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
-import LinkingConfiguration from './LinkingConfiguration';
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+import ModalScreen from "../screens/ModalScreen";
+import NotFoundScreen from "../screens/NotFoundScreen";
+import HomeTab from "../screens/HomeTab";
+import SearchTab from "../screens/SearchTab";
+import ProfileTab from "../screens/ProfileTab";
+import {
+  RootStackParamList,
+  RootTabParamList,
+  RootTabScreenProps,
+} from "../types";
+import LinkingConfiguration from "./LinkingConfiguration";
 
-import HomeIcon from '../assets/images/common/ic_home.png';
-import SearchIcon from '../assets/images/common/ic_search.png';
-import FoucesdIcon from '../assets/images/common/ic_focused.png';
-import PlusIcon from '../assets/images/common/ic_plus.png';
+import HomeIcon from "../assets/images/common/ic_home.png";
+import SearchIcon from "../assets/images/common/ic_search.png";
+import ProfileIcon from "../assets/images/common/ic_user.png";
+import FoucesdIcon from "../assets/images/common/ic_focused.png";
+import PlusIcon from "../assets/images/common/ic_plus.png";
 
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+export default function Navigation({
+  colorScheme,
+}: {
+  colorScheme: ColorSchemeName;
+}) {
   return (
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme = {DarkTheme}>
+    <NavigationContainer linking={LinkingConfiguration} theme={DarkTheme}>
       <RootNavigator />
     </NavigationContainer>
   );
@@ -43,9 +61,17 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 function RootNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-      <Stack.Group screenOptions={{ presentation: 'modal' }}>
+      <Stack.Screen
+        name="Root"
+        component={BottomTabNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="NotFound"
+        component={NotFoundScreen}
+        options={{ title: "Oops!" }}
+      />
+      <Stack.Group screenOptions={{ presentation: "modal" }}>
         <Stack.Screen name="Modal" component={ModalScreen} />
       </Stack.Group>
     </Stack.Navigator>
@@ -65,82 +91,112 @@ function BottomTabNavigator() {
     <BottomTab.Navigator
       initialRouteName="TabOne"
       screenOptions={{
-        tabBarActiveBackgroundColor: 'black',
-        tabBarInactiveBackgroundColor: 'black',
-        tabBarLabel: ''
-      }}>
+        tabBarActiveBackgroundColor: "black",
+        tabBarInactiveBackgroundColor: "black",
+        tabBarLabel: "",
+      }}
+    >
       <BottomTab.Screen
         name="TabOne"
         component={HomeTab}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'MOMENTOON',
+        options={({ navigation }: RootTabScreenProps<"TabOne">) => ({
+          title: "MOMENTOON",
           headerStyle: {
-            backgroundColor: 'black',
+            backgroundColor: "black",
           },
           headerTitleStyle: {
-            fontWeight: 'bold',
+            fontWeight: "bold",
             fontSize: 28,
           },
-          headerTitleAlign: 'left',
-          tabBarIcon: ({focused}) => { 
+          headerTitleAlign: "left",
+          tabBarIcon: ({ focused }) => {
             return (
               <>
-              <Image source={HomeIcon} style={styles.tabIcon}></Image>
-              <Image source={FoucesdIcon} style={focused? styles.indicator : styles.hideIndicator}></Image>
+                <Image source={HomeIcon} style={styles.tabIcon}></Image>
+                <Image
+                  source={FoucesdIcon}
+                  style={focused ? styles.indicator : styles.hideIndicator}
+                ></Image>
               </>
-              )},
+            );
+          },
           headerRight: () => (
             <Pressable
-              onPress={() => navigation.navigate('Modal')}
+              onPress={() => navigation.navigate("Modal")}
               style={({ pressed }) => ({
                 opacity: pressed ? 0.5 : 1,
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center'
-              })}>
-              <Image source={PlusIcon} style={{width: 18, height: 18, marginRight: 3}}/>
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              })}
+            >
+              <Image
+                source={PlusIcon}
+                style={{ width: 18, height: 18, marginRight: 3 }}
+              />
               <Text style={styles.createBtn}>create</Text>
             </Pressable>
           ),
         })}
       />
       <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoScreen}
+        name="SearchTab"
+        component={SearchTab}
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({focused}) => { 
+          title: "Search",
+          tabBarIcon: ({ focused }) => {
             return (
               <>
-              <Image source={SearchIcon} style={styles.tabIcon}></Image>
-              <Image source={FoucesdIcon} style={focused? styles.indicator : styles.hideIndicator}></Image>
-              </>  
-            )},
+                <Image source={SearchIcon} style={styles.tabIcon}></Image>
+                <Image
+                  source={FoucesdIcon}
+                  style={focused ? styles.indicator : styles.hideIndicator}
+                ></Image>
+              </>
+            );
+          },
+        }}
+      />
+      <BottomTab.Screen
+        name="ProfileTab"
+        component={ProfileTab}
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused }) => {
+            return (
+              <>
+                <Image source={ProfileIcon} style={styles.tabIcon}></Image>
+                <Image
+                  source={FoucesdIcon}
+                  style={focused ? styles.indicator : styles.hideIndicator}
+                ></Image>
+              </>
+            );
+          },
         }}
       />
     </BottomTab.Navigator>
   );
 }
 
-
 const styles = StyleSheet.create({
   tabIcon: {
     width: 32,
     height: 32,
-    marginTop: 10
+    marginTop: 10,
   },
   indicator: {
     marginTop: 3,
     width: 6,
-    height: 6
+    height: 6,
   },
   hideIndicator: {
-    display: 'none'
+    display: "none",
   },
   createBtn: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     fontSize: 20,
-    marginRight: 15
-  }
+    marginRight: 15,
+  },
 });
