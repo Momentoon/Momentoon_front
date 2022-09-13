76b557b8df9c5969f058e53fec96a3a1531f78b9
xongjaemin <woals99@gmail.com>
2022-09-14 01:54:32 +0900

    feat: 프레임 추가 / 삭제

--- screens/CreateModal.tsx
+++ screens/CreateModal.tsx
@@ -17,6 +17,7 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
 import { SimpleLineIcons } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 
 export default function CreateModal() {
   /*
@@ -26,8 +27,10 @@ export default function CreateModal() {
     });
   }, []);*/
 
-  const dimensions = Dimensions.get("window");
-  const screenHeight = dimensions.height;
+  //const dimensions = Dimensions.get("window");
+  //const screenHeight = dimensions.height;
+
+  const [currentMode, setCurrentMode] = useState(0);
 
   const [menuList, setMenuList] = useState([
     {
@@ -56,6 +59,7 @@ export default function CreateModal() {
 
   //const [currentFrameNum, setCurrentFrameNum] = useState(4);
 
+  /*갤러리에서 이미지 가져오기*/
   const choosePhotoFromLibrary = (i: number) => {
     console.log("이미지 선택");
     ImagePicker.openPicker({
@@ -70,117 +74,267 @@ export default function CreateModal() {
     });
   };
 
+  /*프레임추가*/
+  const addFrame = () => {
+    var temp = [...imageList];
+    temp.push("");
+    setImageList(temp);
+  };
+
+  /*프레임 삭제*/
+  const deleteFrame = (i: number) => {
+    var temp = [...imageList];
+    temp.splice(i, 1);
+    setImageList(temp);
+  };
+
   return (
     <View style={styles.container}>
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
 
-      <ScrollView style={{ width: "100%", backgroundColor: "black" }}>
-        <View
-          style={{
-            width: "100%",
-            backgroundColor: "black",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            justifyContent: "space-evenly",
-            marginTop: 50,
-          }}
-        >
-          {imageList.map((a, i) => (
-            <TouchableOpacity
-              onPress={() => {
-                choosePhotoFromLibrary(i);
-              }}
-            >
-              <View
-                style={{
-                  width: 160,
-                  height: 160,
-                  marginBottom: 17,
-                  borderRadius: 10,
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                {a === "" ? (
-                  <FontAwesome name="camera" size={50} color="black" />
-                ) : (
-                  <Image
-                    source={{ uri: a }}
-                    style={{ width: "100%", height: "100%", borderRadius: 10 }}
-                  ></Image>
-                )}
-              </View>
-            </TouchableOpacity>
-          ))}
-        </View>
-      </ScrollView>
-
-      <View
-        style={{
-          width: "100%",
-          height: 300,
-          backgroundColor: "black",
-          alignItems: "center",
-        }}
-      >
-        <View
-          style={{
-            width: "100%",
-            backgroundColor: "black",
-            flexDirection: "row",
-            justifyContent: "space-around",
-          }}
-        >
-          {menuList.map((a) => (
+      {currentMode === 0 ? ( //기본 모드
+        <>
+          <ScrollView style={{ width: "100%", backgroundColor: "black" }}>
             <View
               style={{
-                flex: 0.2,
-                aspectRatio: 0.7,
+                width: "100%",
                 backgroundColor: "black",
-                marginTop: 10,
+                flexDirection: "row",
+                flexWrap: "wrap",
+                justifyContent: "space-between",
+                marginTop: 50,
+                paddingLeft: "8%",
+                paddingRight: "8%",
               }}
             >
-              <TouchableOpacity>
+              {imageList.map((a, i) => (
+                <>
+                  <TouchableOpacity
+                    onPress={() => {
+                      choosePhotoFromLibrary(i);
+                    }}
+                  >
+                    <View
+                      style={{
+                        width: 160,
+                        height: 160,
+                        marginBottom: 17,
+                        borderRadius: 10,
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      {a === "" ? (
+                        <FontAwesome name="camera" size={50} color="black" />
+                      ) : (
+                        <Image
+                          source={{ uri: a }}
+                          style={{
+                            width: "100%",
+                            height: "100%",
+                            borderRadius: 10,
+                          }}
+                        ></Image>
+                      )}
+                    </View>
+                    {/*
+                    <View
+                      style={{
+                        width: 28,
+                        height: 28,
+                        borderRadius: 14,
+                        position: "absolute",
+                        backgroundColor: "#ADADAD",
+                      }}
+                    ></View>*/}
+                  </TouchableOpacity>
+                </>
+              ))}
+            </View>
+          </ScrollView>
+
+          <View
+            style={{
+              width: "100%",
+              height: 300,
+              backgroundColor: "black",
+              alignItems: "center",
+            }}
+          >
+            <View
+              style={{
+                width: "100%",
+                backgroundColor: "black",
+                flexDirection: "row",
+                justifyContent: "space-around",
+              }}
+            >
+              {menuList.map((a, i) => (
                 <View
                   style={{
-                    aspectRatio: 1,
+                    flex: 0.2,
+                    aspectRatio: 0.7,
+                    backgroundColor: "black",
+                    marginTop: 10,
+                  }}
+                >
+                  <TouchableOpacity
+                    onPress={() => {
+                      setCurrentMode(i + 1);
+                    }}
+                  >
+                    <View
+                      style={{
+                        aspectRatio: 1,
+                        borderRadius: 10,
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      {a.icon}
+                    </View>
+                  </TouchableOpacity>
+                  <View
+                    style={{
+                      backgroundColor: "black",
+                      alignItems: "center",
+                      marginTop: 10,
+                    }}
+                  >
+                    <Text style={{ color: "white", fontWeight: "bold" }}>
+                      {a.title}
+                    </Text>
+                  </View>
+                </View>
+              ))}
+            </View>
+            <TouchableOpacity
+              style={{
+                width: "90%",
+                height: 48,
+                backgroundColor: "#C4C4C4",
+                marginTop: 25,
+                borderRadius: 10,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ fontSize: 24, fontWeight: "bold" }}>Complete</Text>
+            </TouchableOpacity>
+          </View>
+        </>
+      ) : currentMode === 1 ? ( //프레임 편집 모드
+        <>
+          <ScrollView style={{ width: "100%", backgroundColor: "black" }}>
+            <View
+              style={{
+                width: "100%",
+                backgroundColor: "black",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                justifyContent: "space-between",
+                marginTop: 50,
+                paddingLeft: "8%",
+                paddingRight: "8%",
+              }}
+            >
+              {imageList.map((a, i) => (
+                <>
+                  <View
+                    style={{
+                      width: 160,
+                      height: 160,
+                      marginBottom: 17,
+                      borderRadius: 10,
+                    }}
+                  >
+                    <View
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: 10,
+                      }}
+                    >
+                      {a === "" ? (
+                        <FontAwesome name="camera" size={50} color="black" />
+                      ) : (
+                        <Image
+                          source={{ uri: a }}
+                          style={{
+                            width: "100%",
+                            height: "100%",
+                            borderRadius: 10,
+                          }}
+                        ></Image>
+                      )}
+                    </View>
+                    <TouchableOpacity
+                      style={{
+                        width: 28,
+                        height: 28,
+                        borderRadius: 14,
+                        position: "absolute",
+                        backgroundColor: "#ADADAD",
+                        alignSelf: "flex-end",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                      onPress={() => {
+                        deleteFrame(i);
+                      }}
+                    >
+                      <Feather name="minus" size={24} color="black" />
+                    </TouchableOpacity>
+                  </View>
+                </>
+              ))}
+
+              <TouchableOpacity onPress={addFrame}>
+                <View
+                  style={{
+                    width: 160,
+                    height: 160,
+                    marginBottom: 17,
                     borderRadius: 10,
                     justifyContent: "center",
                     alignItems: "center",
                   }}
                 >
-                  {a.icon}
+                  <FontAwesome name="plus" size={50} color="black" />
                 </View>
               </TouchableOpacity>
-              <View
-                style={{
-                  backgroundColor: "black",
-                  alignItems: "center",
-                  marginTop: 10,
-                }}
-              >
-                <Text style={{ color: "white", fontWeight: "bold" }}>
-                  {a.title}
-                </Text>
-              </View>
             </View>
-          ))}
-        </View>
-        <TouchableOpacity
-          style={{
-            width: "90%",
-            height: 48,
-            backgroundColor: "#C4C4C4",
-            marginTop: 25,
-            borderRadius: 10,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Text style={{ fontSize: 24, fontWeight: "bold" }}>Complete</Text>
-        </TouchableOpacity>
-      </View>
+          </ScrollView>
+          <View
+            style={{
+              width: "100%",
+              height: 200,
+              backgroundColor: "black",
+              alignItems: "center",
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                width: "90%",
+                height: 48,
+                backgroundColor: "#C4C4C4",
+                marginTop: 25,
+                borderRadius: 10,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onPress={() => {
+                setCurrentMode(0);
+              }}
+            >
+              <Text style={{ fontSize: 24, fontWeight: "bold" }}>Complete</Text>
+            </TouchableOpacity>
+          </View>
+        </>
+      ) : null}
     </View>
   );
 }
