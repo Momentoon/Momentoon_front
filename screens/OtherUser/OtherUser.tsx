d3050642d4e1c8c6090358bc0337393f6c6cabdd
xongjaemin <woals99@gmail.com>
2023-03-27 22:24:01 +0900

    dev: 팔로우/언팔로우 기능

--- screens/OtherUser/OtherUser.tsx
+++ screens/OtherUser/OtherUser.tsx
@@ -39,7 +39,17 @@ export default function OtherUser({
     { name: "Following", number: 0 },
   ]);
 
+  const [followingOrNot, setFollowingOrNot] = useState(false);
+
+  const [myUserIdx, setMyUserIdx] = useState(0);
+  const [myFollowingNum, setMyFollowingNum] = useState(0);
+  const [otherUserIdx, setOtherUserIdx] = useState(0);
+
+  const [followerIdx, setFollowerIdx] = useState(0);
+  const [followingIdx, setFollowingIdx] = useState(0);
+
   useEffect(() => {
+    //이미지 리스트 가져오기
     firebase_db
       .ref("/images")
       .orderByChild("user")
@@ -47,17 +57,90 @@ export default function OtherUser({
       .once("value")
       .then((snapshot) => {
         if (snapshot.val() != null) {
-          console.log(snapshot.val());
+          //console.log(snapshot.val());
           setImageList(Object.values(snapshot.val()));
         }
       });
-  }, []);
+
+    //내가 해당 유저 팔로우 중인지 아닌지 확인
+    firebase_db
+      .ref("/users")
+      .orderByChild("email")
+      .equalTo(reduxState.currentUser)
+      .once("value")
+      .then((snapshot) => {
+        var data = Object.values(snapshot.val())[0];
+        var followingList = data.following;
+        //setMyFollowingNum(followingList.length());
+        if (followingList.includes(route.params.userName)) {
+          //console.log(followingList.indexOf(route.params.userName));
+          setFollowingIdx(followingList.indexOf(route.params.userName));
+          setFollowingOrNot(true);
+        }
+        setMyUserIdx(data.idx);
+        setMyFollowingNum(followingList.length);
+      });
+
+    firebase_db
+      .ref("/users")
+      .orderByChild("email")
+      .equalTo(route.params.userName)
+      .once("value")
+      .then((snapshot) => {
+        var data = Object.values(snapshot.val())[0];
+        var followerList = data.followers;
+
+        setOtherUserIdx(data.idx);
+        console.log(otherUserIdx);
+
+        if (followerList.includes(reduxState.currentUser)) {
+          //console.log(followerList.indexOf(reduxState.currentUser));
+          setFollowerIdx(followerList.indexOf(reduxState.currentUser));
+        }
+
+        let temp = [...indicator];
+        temp[1].number = data.followers.length - 1;
+        temp[2].number = data.following.length - 1;
+
+        setIndicator(temp);
+      });
+  }, [, followingOrNot]);
 
   useEffect(() => {
     let temp = [...indicator];
     temp[0].number = imageList.length;
     setIndicator(temp);
   }, [imageList]);
+
+  const followUser = async () => {
+    const reference = firebase_db.ref();
+    const myRef = reference.child(
+      `/users/${myUserIdx}/following/${myFollowingNum}`
+    );
+    myRef.set(route.params.userName);
+
+    const otherRef = reference.child(
+      `/users/${otherUserIdx}/followers/${indicator[1].number + 1}`
+    );
+    otherRef.set(reduxState.currentUser);
+
+    setFollowingOrNot(true);
+  };
+
+  const unFollowUser = () => {
+    const reference = firebase_db.ref();
+    const myRef = reference.child(
+      `/users/${myUserIdx}/following/${followingIdx}`
+    );
+    myRef.remove();
+
+    const otherRef = reference.child(
+      `/users/${otherUserIdx}/followers/${followerIdx}`
+    );
+    otherRef.remove();
+
+    setFollowingOrNot(false);
+  };
 
   return (
     <View style={styles.container}>
@@ -101,11 +184,19 @@ export default function OtherUser({
             justifyContent: "space-around",
           }}
         ></View>
-        <TouchableOpacity style={{ width: "90%" }}>
-          <View style={styles.followBtn}>
-            <Text style={styles.followText}>Follow</Text>
-          </View>
-        </TouchableOpacity>
+        {followingOrNot ? (
+          <TouchableOpacity style={{ width: "90%" }} onPress={unFollowUser}>
+            <View style={styles.unFollowBtn}>
+              <Text style={styles.unFollowText}>Unfollow</Text>
+            </View>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity style={{ width: "90%" }} onPress={followUser}>
+            <View style={styles.followBtn}>
+              <Text style={styles.followText}>Follow</Text>
+            </View>
+          </TouchableOpacity>
+        )}
       </View>
       <View style={{ backgroundColor: "black" }}>
         <ScrollView
@@ -209,6 +300,19 @@ const styles = StyleSheet.create({
   },
   followText: {
     color: "white",
+    fontSize: 20,
+    fontWeight: "700",
+  },
+  unFollowBtn: {
+    height: 45,
+    borderRadius: 10,
+    width: "100%",
+    backgroundColor: "#C4C4C4",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  unFollowText: {
+    color: "black",
     fontSize: 20,
     fontWeight: "700",
   },
